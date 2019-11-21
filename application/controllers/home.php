ff801cddbe3a92e7af03bb58cb550395f5eee0b2
@astynabilah <nabilahasty6@gmail.com>
2019-11-21 21:47:28 +0900

    view home done

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,6 +4,7 @@ class home extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('header');
 		$this->load->view('home_V');
 	}
 }
--- application/views/home_V.php
+++ application/views/home_V.php
@@ -1,3 +1,114 @@
 <!DOCTYPE html>
-  Welcome
+    <section style="background-color: #7AB3B0; height: 600px">
+      <h1 class="site-heading text-center text-Black d-none d-lg-block" style="height: 30px;">
+        <br/>
+          <span class="site-heading-lower">COLORBLIND BUDDY</span><br/>
+          <div style="font-size:50%">An Aid For The Colorblind</div>
+          <hr style="width: 600px; background-color: gray">
+      </h1>
+      <div class="container" style="width: 700px; height: 650px; background-image: url('https://i.pinimg.com/originals/96/16/71/961671a0d15c33d419b8364e1345ccd2.png'); background-size: 100% 100%">'
+        <div class="container" style="margin-top: 200px ;width: 500px; height: 250px;">
+          <h2>We're ready to help you!</h2>
+            Color is one of the most important things in human’s life which defines the characteristics of an image. Unfortunately, not all people can see the difference between these colors. 
+            Are you one of them?
+            Then you are in the right place!
+            We offer this app to solve your problem, and make you see the difference in the colors easily. 
+          </div>
+        </div>
+    </section>
+    <section style="background-image: linear-gradient(#7AB3B0, #ddb075)">  
+      <br/>
+      <br/>
+    </section>
+    <section class="content-section text-white text-center" style="background-color: #ddb075; height: 600px" id="feature">
+    <div class="container">
+      <div class="content-section-heading">
+        <br/>
+        <h2 style="color: black">Our Features</h2>
+        <div style="color: black">Just click one of features below!</div>
+        <br/>
+      </div>
+      <div class="row" style="color: black">
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" id="colordetector">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/1492/PNG/128/art-pallete_102830.png">
+          </span>
+          <h4>
+            <strong>Color Detector</strong>
+          </h4>
+          <p class="text-faded mb-0">Upload your image and let us tell you the color!</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" id="colorfinder">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src ="https://icon-icons.com/icons2/1393/PNG/128/-color-4_96736.png">
+          </span>
+          <h4>
+            <strong>Color Finder</strong>
+          </h4>
+          <p class="text-faded mb-0">Upload an image and tell us what do you want to find!</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-md-0" id="colorblindtest">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/1918/PNG/128/iconfinder-document03-1622833_121957.png">
+          </span>
+          <h4>
+            <strong>Color Blind Test</strong>
+          </h4>
+          <p class="text-faded mb-0">Check if you're colorbinded or not by answering questions!</p>
+        </div>
+        <div class="col-lg-3 col-md-6" id="quizzes">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/1805/PNG/128/4230548-confusion-doubt-frustration-question_115011.png">
+          </span>
+          <h4>
+            <strong>Quizzes</strong>
+          </h4>
+          <p class="text-faded mb-0">We want to know how much you know about colorblind!</p>
+        </div>
+      </div>
+      <div class="row" style="color: black">
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" id="colorharmony">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/624/PNG/128/RGB_Circle_1-80_icon-icons.com_57282.png">
+          </span>
+          <br/>
+          <h4>
+            <strong>Color Harmony</strong>
+          </h4>
+          <p class="text-faded mb-0">Great for finding gradient colors!</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" id="articles">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/516/PNG/128/read_book_study_icon-icons.com_51077.png">
+          </span>
+          <h4>
+            <strong>Articles</strong>
+          </h4>
+          <p class="text-faded mb-0">Increase your knowledge about colorblind!</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-md-0" id="clothingguide">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+            <img src="https://icon-icons.com/icons2/1794/PNG/128/shirt_114788.png">
+          </span>
+          <h4>
+            <strong>Style Me Up</strong>
+          </h4>
+          <p class="text-faded mb-0">We make sure you use the right color of your clothes!</p>
+        </div>
+        <div class="col-lg-3 col-md-6" id="eyedoctor">
+          <span class="service-icon rounded-circle mx-auto mb-3">
+          </br>
+            <img src="https://icon-icons.com/icons2/1393/PNG/128/eye-3_96721.png">
+          </span>
+          <h4>
+            <br/>
+            <strong>Eye Doctor Near Me</strong>
+          </h4>
+          <p class="text-faded mb-0">In case you need future examination</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  </body>
+
 </html>
