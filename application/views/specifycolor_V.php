992983412a5aec65e693012dd96441b4434e8e65
putuwinda <windaardiyanti@student.telkomuniversity.ac.id>
2019-12-03 22:04:12 +0700

    colorspecify(not yet)

---
+++ application/views/specifycolor_V.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+
+  <head>
+    <meta charset="utf-8">
+
+
+    <script src="https://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="#">
+    <script type="text/javascript" language="javacript" src="specifycolor.js.js"></script>
+    <title> COLOR IDENTIFY </title>
+
+  </head>
+  <body>
+    <div class="main">
+       <h1 id="title"> IDENTIFY THIS COLOR!</h1>
+       <form method="post" action="" enctype="multipart/form-data">
+         <br>
+           <p> PLEASE CHOOSE A PICTURE!</p>
+           <div id="image-preview-before"></div>
+           <input type="file" id="image-file" name="test">
+           <p id="file-info"></p>
+       </form>
+       <div id="finalDisplay"></div>
+       <div class="push"></div>
+   </div>
+
+
+  </body>
+</html>
